90847ddefcb36e969aa9e668162e22b7858dc9ec
Evgeny Kalinin <user8755@yandex.ru>
2023-04-08 22:55:30 +0500

    add router user and card

--- controllers/cards.js
+++ controllers/cards.js
@@ -20,3 +20,24 @@ module.exports.createCard = (req, res) => {
     .then((card) => res.send({ data: card }))
     .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
 };
+
+module.exports.likeCard = (req, res) => {
+  console.log(req.params)
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true }
+  )
+    .then((card) => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true }
+  )
+    .then((card) => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const {getCards, delCardsById, createCard} = require("../controllers/cards")
+const {getCards, delCardsById, createCard, likeCard, dislikeCard} = require("../controllers/cards")
 
 router.get("/", getCards);
 router.delete("/:cardId", delCardsById);
 router.post("/", createCard);
-
+router.put("/:cardId/likes", likeCard);
+router.delete("/:cardId/likes", dislikeCard);
 
 module.exports = router;
