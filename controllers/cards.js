65c594c8c3285639b748f5bce1b53bdc23d9a6ac
User8755 <110420113+User8755@users.noreply.github.com>
2023-05-10 22:24:58 +0500

    fix send jsom

--- controllers/cards.js
+++ controllers/cards.js
@@ -32,7 +32,7 @@ module.exports.delCardsById = (req, res, next) => {
 module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.send({ data: card }))
+    .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequestError('Карточка не создана'));
