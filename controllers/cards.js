b87a6a7679c72719cff0cb4daa42f8d8377c2e07
User8755 <110420113+User8755@users.noreply.github.com>
2023-05-10 21:10:28 +0500

    fix send card

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,7 +5,7 @@ const ForbiddenError = require('../errors/forbidden');
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .then((card) => res.send({ data: card }))
+    .then((card) => res.send(card))
     .catch(next);
 };
 
